eb03c41fe474394cabe80c59e208bc814b177802
Yeison1994s <yeisonestivenlopezimbachi@gmail.com>
2021-07-14 10:59:58 -0500

    Initial commit relacion table detalle

---
+++ src/main/java/com/data/onetoone02/OneToOne02Application.java
@@ -0,0 +1,13 @@
+package com.data.onetoone02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOne02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOne02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/data/onetoone02/OneToOne02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.onetoone02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOne02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
